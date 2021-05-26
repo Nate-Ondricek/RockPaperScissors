5fc9dd0b7e4e5aaadbbe918c48ca4f07d6eed5d5
Nate-Ondricek <no766@stern.nyu.edu>
2021-05-25 20:21:11 -0400

    user input created

--- game.py
+++ game.py
@@ -1,2 +1,7 @@
 print("Rock, Paper, Scissors, Shoot!") 
 
+user_choice = input("Choose your weapon 'rock', 'paper', 'scissors':")
+
+print("You chose: ", user_choice)
+print("May the odds be ever in your favor")
+
