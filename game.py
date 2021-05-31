80bb2f267776dfadf49f00529fd0ee47dbff137d
Nate-Ondricek <no766@stern.nyu.edu>
2021-05-31 14:01:30 -0400

    cleaned up unnecessary comments
    Final Version (v1)

--- game.py
+++ game.py
@@ -9,15 +9,6 @@ dotenv.load_dotenv()
 
 
 player_name = os.getenv("player_name")
-#print(player_name)
-
-#more details on env variables in slack for 5/27 around 6:40 PM
-
-#update the welcome to the game message so it's saying welcome player name
-# remember player_name is a variable now
-
-#wanna be cool? consider giving an option of doing single game or best out of three
-
 
 print("------------------------")
 print("Welcome",player_name,"to my Rock-Paper-Scissors game!") 
@@ -26,15 +17,6 @@ print("------------------------")
 user_choice = input("Choose your weapon 'rock', 'paper', or 'scissors':")
 
 print("You chose: ", user_choice)
-
-
-#Vvalidate the input such that only if it is one of the expected values
-#...will we continue with the rest of the program
-#...otherwise we'll stop the program before it tries to do anything else
-#...and we'll ask the user to run the program again
-
-#Consider adding some --------- to the code to space things out a bit nicer
-
 
 if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
     print("May the odds be ever in your favor")
@@ -49,7 +31,8 @@ computer_choice = random.choice(valid_options)
 print("Robot Overlord Chose: ", computer_choice)
 print("------------------------")
 
-#Code suggestion of making user_choice==comp_choice attributed to Steven S.
+# Draw
+# Code suggestion of making user_choice==comp_choice attributed to Steven S.
 
 if (computer_choice==user_choice):
     print("It's a draw!")
